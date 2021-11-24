0d37fcf500e313fd768babba85a4692a342b5f2e
KhalidAgha-2000 <mkhalid.k.agha@gmail.com>
2021-11-24 06:16:11 +0200

    Step_2 Done!

---
+++ javascript-basics-01/Step_2.js
@@ -0,0 +1,6 @@
+var Name = "Khalid";
+var surname = "Agha";
+var city = "Tripoli";
+document.getElementById("box").innerHTML =
+ "Name : " + Name + "<br>" + "Surname :" + surname + "<br>" + "City : " + city;
+
