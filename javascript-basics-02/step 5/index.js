400292ac5ad14f788ad23506b12ced04e4c348c1
KhalidAgha-2000 <mkhalid.k.agha@gmail.com>
2021-11-29 00:40:20 +0200

    step5 done

---
+++ javascript-basics-02/step 5/index.js
@@ -0,0 +1,16 @@
+function myOver(img) {
+    if (img == "image1") {
+        document.querySelector("#image1").style.border = "10px solid red";
+    } else if (img == "image2") {
+        document.querySelector("#image2").style.border = "10px solid green";
+    } else if (img == "image3") {
+        document.querySelector("#image3").style.border = "10px solid yellow";
+    }
+    else if (img == "image4") {
+        document.querySelector("#image4").style.border = "10px solid blue";
+    }
+    else if (img == "image5") {
+        document.querySelector("#image5").style.border = "10px solid pink";
+    }
+}
+
