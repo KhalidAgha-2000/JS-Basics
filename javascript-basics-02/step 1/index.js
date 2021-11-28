2e186c6f4d5288df21ce8e74c6e193261fc47bf7
KhalidAgha-2000 <mkhalid.k.agha@gmail.com>
2021-11-29 00:32:19 +0200

    step1 done

---
+++ javascript-basics-02/step 1/index.js
@@ -0,0 +1,3 @@
+document.querySelector("#image1").addEventListener("mouseover", function mouseOver() {
+    if (document.querySelector("#image1")) { document.querySelector("#image1").style.border = "10px solid red"; }
+});
