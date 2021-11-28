2bed61041643e06c71b1da92e8550c83b07f83ca
KhalidAgha-2000 <mkhalid.k.agha@gmail.com>
2021-11-29 00:42:26 +0200

    step6 done

---
+++ javascript-basics-02/step 6/index.js
@@ -0,0 +1,32 @@
+function myOver(img) {
+    if (img == "image1") {
+        document.querySelector("#image1").style.width = "250px";
+    } else if (img == "image2") {
+        document.querySelector("#image2").style.width = "250px";
+    } else if (img == "image3") {
+        document.querySelector("#image3").style.width = "250px";
+    }
+    else if (img == "image4") {
+        document.querySelector("#image4").style.width = "250px";
+    }
+    else if (img == "image5") {
+        document.querySelector("#image5").style.width = "250px";
+    }
+}
+
+
+function myOut(img) {
+    if (img == "image1") {
+        document.querySelector("#image1").style.width = "110px";
+    } else if (img == "image2") {
+        document.querySelector("#image2").style.width = "110px";
+    } else if (img == "image3") {
+        document.querySelector("#image3").style.width = "110px";
+    }
+    else if (img == "image4") {
+        document.querySelector("#image4").style.width = "110px";
+    }
+    else if (img == "image5") {
+        document.querySelector("#image5").style.width = "110px";
+    }
+}
